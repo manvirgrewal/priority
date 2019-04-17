1cb89cfd9c82c956c1955d63aabc95491b634bc8
AliveAgain <15mg7@caslab.queensu.ca>
2019-04-17 00:37:08 -0400

    Creating Tasks works via GUI, next: "add classes"

--- client/src/components/CreateTask.vue
+++ client/src/components/CreateTask.vue
@@ -1,55 +1,70 @@
 <template>
   <div>
-    <panel title="Create">
-       <v-text-field
-        label="Task Name"
-        v-model="task.name"
-        color="green accent-4"
-      >
-      </v-text-field>
-      <v-text-field
-        label="Description"
-        v-model="task.description"
-        color="green accent-4"
-      >
-      </v-text-field>
-      <v-text-field
-        label="Class"
-        v-model="task.class"
-        color="green accent-4"
-      >
-      </v-text-field>
-      <v-menu
-        v-model="task.menu"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-      >
-      <template v-slot:activator="{ on }">
-        <v-text-field
-          v-model="date"
-          label="Due Date"
-          readonly
-          v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker v-model="date" @input="menu = false"></v-date-picker>
-      </v-menu>
-      <v-select
-        flat
-        v-model="task.difficulty"
-        :items="levels"
-        label="Estimated Difficulty"
-      ></v-select>
-      <v-btn outline color="green accent-4"
+    <v-layout align-center justify-center>
+      <v-flex xs7>
+        <panel title="Create">
+          <v-text-field
+            label="Task Name"
+            v-model="task.name"
+            color="green accent-4"
+          >
+          </v-text-field>
+          <v-text-field
+            label="Description"
+            v-model="task.description"
+            color="green accent-4"
+          >
+          </v-text-field>
+          <v-text-field
+            label="Class"
+            v-model="task.class"
+            color="green accent-4"
+          >
+          </v-text-field>
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="task.date"
+              label="Due Date"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="task.date">
+          </v-date-picker>
+          </v-menu>
+          <!-- <v-select
+            flat
+            v-model="task.difficulty"
+            :items="levels"
+            label="Estimated Difficulty"
+          ></v-select> -->
+          <v-slider
+            label="Estimated Difficulty"
+            v-model="task.difficulty"
+            max="10"
+            step="1"
+            thumb-label="always"
+            :thumb-size="24"
+            ticks="always"
+            tick-size="2"
+          ></v-slider>
+          <v-btn outline color="green accent-4"
             @click="add">
             ADD
-      </v-btn>
-    </panel>
+          </v-btn>
+        </panel>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -70,8 +85,8 @@ export default {
       name: '',
       description: '',
       class: '',
-      date: null,
-      difficulty: 1,
+      date: this.date,
+      difficulty: 0,
       isDone: false
     }
   }),
--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,102 +1,106 @@
 <template>
   <div>
-    <panel title="Tasks">
-      <v-text-field
-        v-model="task"
-        label="What are you working on?"
-        solo
-        @keydown.enter="create"
-      >
-      <v-fade-transition v-slot:append>
-          <v-icon
-            v-if="task"
-            @click="create"
+    <v-layout align-center justify-center>
+      <v-flex xs7>
+        <panel title="Tasks">
+          <v-text-field
+            v-model="task"
+            label="What are you working on?"
+            solo
+            @keydown.enter="create"
           >
-            add_circle
-          </v-icon>
-        </v-fade-transition>
-      </v-text-field>
-
-      <h2 class="display-1 success--text pl-3">
-        Tasks:&nbsp;
-        <v-fade-transition leave-absolute>
-          <span :key="`tasks-${tasks.length}`">
-            {{ tasks.length }}
-          </span>
-        </v-fade-transition>
-      </h2>
-
-      <v-divider class="mt-3"></v-divider>
-
-      <v-layout
-        my-1
-        align-center
-      >
-        <strong class="mx-3 info--text text--darken-3">
-          Remaining: {{ remainingTasks }}
-        </strong>
-
-        <v-divider vertical></v-divider>
-
-        <strong class="mx-3 black--text">
-          Completed: {{ completedTasks }}
-        </strong>
-
-        <v-spacer></v-spacer>
-
-        <v-progress-circular
-          :value="progress"
-          class="mr-2"
-        ></v-progress-circular>
-      </v-layout>
-
-      <v-divider class="mb-3"></v-divider>
-
-      <v-card v-if="tasks.length > 0">
-        <v-slide-y-transition
-          class="py-0"
-          group
-          tag="v-list"
-        >
-        <template v-for="(task, i) in tasks">
-          <v-divider
-            v-if="i !== 0"
-            :key="`${i}-divider`"
-          ></v-divider>
-
-          <v-list-tile :key="`${i}-${task.text}`">
-            <v-list-tile-action>
-              <v-checkbox
-                v-model="task.done"
-                color="info darken-3"
+          <v-fade-transition v-slot:append>
+              <v-icon
+                v-if="task"
+                @click="create"
               >
-                <template v-slot:label>
-                  <div
-                    :class="task.done && 'grey--text' || 'text--primary'"
-                    class="ml-3"
-                    v-text="task.text"
-                  ></div>
-                </template>
-              </v-checkbox>
-            </v-list-tile-action>
+                add_circle
+              </v-icon>
+            </v-fade-transition>
+          </v-text-field>
+
+          <h2 class="display-1 success--text pl-3">
+            Tasks:&nbsp;
+            <v-fade-transition leave-absolute>
+              <span :key="`tasks-${tasks.length}`">
+                {{ tasks.length }}
+              </span>
+            </v-fade-transition>
+          </h2>
+
+          <v-divider class="mt-3"></v-divider>
+
+          <v-layout
+            my-1
+            align-center
+          >
+            <strong class="mx-3 info--text text--darken-3">
+              Remaining: {{ remainingTasks }}
+            </strong>
+
+            <v-divider vertical></v-divider>
+
+            <strong class="mx-3 black--text">
+              Completed: {{ completedTasks }}
+            </strong>
 
             <v-spacer></v-spacer>
 
-            <v-scroll-x-transition>
-              <v-icon
-                v-if="task.done"
-                color="success"
-              >
-                check
-              </v-icon>
-            </v-scroll-x-transition>
-          </v-list-tile>
-        </template>
-      </v-slide-y-transition>
-    </v-card>
-    <br>
-    <br>
-  </panel>
+            <v-progress-circular
+              :value="progress"
+              class="mr-2"
+            ></v-progress-circular>
+          </v-layout>
+
+          <v-divider class="mb-3"></v-divider>
+
+          <v-card v-if="tasks.length > 0">
+            <v-slide-y-transition
+              class="py-0"
+              group
+              tag="v-list"
+            >
+            <template v-for="(task, i) in tasks">
+              <v-divider
+                v-if="i !== 0"
+                :key="`${i}-divider`"
+              ></v-divider>
+
+              <v-list-tile :key="`${i}-${task.text}`">
+                <v-list-tile-action>
+                  <v-checkbox
+                    v-model="task.done"
+                    color="info darken-3"
+                  >
+                    <template v-slot:label>
+                      <div
+                        :class="task.done && 'grey--text' || 'text--primary'"
+                        class="ml-3"
+                        v-text="task.text"
+                      ></div>
+                    </template>
+                  </v-checkbox>
+                </v-list-tile-action>
+
+                <v-spacer></v-spacer>
+
+                <v-scroll-x-transition>
+                  <v-icon
+                    v-if="task.done"
+                    color="success"
+                  >
+                    check
+                  </v-icon>
+                </v-scroll-x-transition>
+              </v-list-tile>
+            </template>
+          </v-slide-y-transition>
+        </v-card>
+        <br>
+        <br>
+      </panel>
+    </v-flex>
+  </v-layout>
 </div>
 </template>
 
@@ -121,7 +125,17 @@ export default {
     remainingTasks () {
       return this.tasks.length - this.completedTasks
     }
-  }
+  },
+  methods: {
+      create () {
+        this.tasks.push({
+          done: false,
+          text: this.task
+        })
+
+        this.task = null
+      }
+    }
 }
 </script>
 
